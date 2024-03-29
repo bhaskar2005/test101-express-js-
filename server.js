28e30ffc9659c96c89dcaa070375a0cda800f806
bhaskar2005 <bhaskarroyofficial2005@gmail.com>
2024-03-30 00:34:09 +0530

    home page connect with login & register page

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+
+app.get('/login', (req, res) => {
+    res.render('loginStudent');
+});
+
+app.get('/register', (req, res) => {
+    res.render('registerStudent');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
